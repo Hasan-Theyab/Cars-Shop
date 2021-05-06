56f4a61b4a42bedf3e4c238fd38e32df5f932a8a
HHDOOP <>
2021-05-06 15:18:02 +0000

    Exporting the modification to github

--- javascript/app.js
+++ javascript/app.js
@@ -8,54 +8,40 @@ if (age >= 20) {
 } else if (age <= 20) {
   alert("Age is younger than 20");
 } else {
-  alert("Please,Enter numbers only.");
+  alert("Please,enter numbers only.");
 }
-
-var favouriteCar = prompt("What do like more Mercedes or Ford or Toyota?");
+//This code to print the car's image on screen
+var favouriteCar = prompt("What do you like more Mercedes or Ford or Toyota.\nNote: This question is to show your favourite car's picture in the bottom of the site?");
 if (favouriteCar == 'Mercedes') {
   document.write('<img src="https://www.arabgt.com/wp-content/uploads/2019/12/2021-mercedes-s-class.jpg" width="250px">');
 } else if (favouriteCar === 'Ford') {
   document.write('<img src="https://www.arabgt.com/wp-content/uploads/2020/04/%D9%81%D9%88%D8%B1%D8%AF-F-150-2021-1.jpg" width="250px">');
 } else if (favouriteCar === 'Toyota') {
   document.write('<img src="https://1.bp.blogspot.com/-xtIIyB5icWk/Xxn8s2D7lKI/AAAAAAAAHVY/ympfHyUnbowPe2aL-PbOCgACUXvIqajHgCLcBGAsYHQ/s1600/untitled_optimized%2B%25281%2529.jpg" width="250px">');
-
-<<<<<<< HEAD
 } else {
-  document.write('<h3> please only write Mercedes or Ford or Toyota next time</h3>');
+  alert('Please, only write Mercedes or Ford or Toyota next time');
 }
 
 
 
-var favouriteCar = prompt('What do you like more Mercedes or Ford or Toyota?');
+//This code to loop entering until we gain acceptable value
 
-while (favouriteCar === 'Mercedes' || favouriteCar === 'Ford' || favouriteCar === 'Toyota') {
-  if (favouriteCar === 'Mercedes') {
-    document.write('<h3>Welcome to our website This is Mercedes</h3>');
-  }
-  else if (favouriteCar === 'Ford') {
-    alert('Hello,welcome to our website,here is the Ford section');
-  }
-  else if (favouriteCar === 'Toyota') {
-    alert('Hello,welcome to our website we have Toyota offers');
-  }
-  else {
-    favouriteCar = prompt('Please only answer with Mercedes or Ford or Toyota');
-  }
+var favouriteCar = prompt('What is your favourite car: Mercedes or Ford or Toyota?\nNote: This question is to check your input is correct or not.');
 
+while (favouriteCar !== 'Mercedes' && favouriteCar !== 'Ford' && favouriteCar !== 'Toyota') {
+  favouriteCar = prompt('Please only answer with Mercedes or Ford or Toyota');
 }
 
 var cars = prompt('How many cars do you want to buy?');
 
-if (cars > 5) {
+if (cars > 0) {
   for (var i = 0; i < cars; i++) {
 
     document.write('<img src="https://www.arabgt.com/wp-content/uploads/2019/12/2021-mercedes-s-class.jpg" width="50px">');
   }
-} else {
-  for (var i = 0; i < cars; i++) {
-    document.write('<img src="https://www.arabgt.com/wp-content/uploads/2019/12/2021-mercedes-s-class.jpg" width="50px">');
-  }
+} if (cars < 0) {
+  alert('Please,enter a positive number only.');
 }
-=======
-}
->>>>>>> 5196ee274f9d16183e38d0a4d8b7fd7533240ab4
+
+
+
